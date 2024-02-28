1f8bc9d83fad1c339f0db5763d48cd1fdbd0d9b7
Kirill Kozlov <space_cell@proton.me>
2024-02-28 23:13:48 +0500

    Init commit

---
+++ inquiry-service/src/test/java/com/iprody08/inquiryservice/InquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iprody08.inquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
