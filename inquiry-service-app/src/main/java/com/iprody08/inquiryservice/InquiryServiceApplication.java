b608370922a803eb8300b6e382831ae730d65178
Alexey Polyakov <sulfur1917@gmail.com>
2024-03-08 21:36:48 +0300

    Restructure project

---
+++ inquiry-service-app/src/main/java/com/iprody08/inquiryservice/InquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iprody08.inquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryServiceApplication.class, args);
+	}
+
+}
