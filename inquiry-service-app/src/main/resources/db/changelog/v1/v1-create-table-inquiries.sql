3ee4d381a8c135790455bea1ffec49d64d7a36ed
pbpgdanovich <sega0172@gmail.com>
2024-03-26 18:52:12 +0300

    test controller and sort

--- inquiry-service-app/src/main/resources/db/changelog/v1/v1-create-table-inquiries.sql
+++ inquiry-service-app/src/main/resources/db/changelog/v1/v1-create-table-inquiries.sql
@@ -1,8 +1,5 @@
 create table inquiries (
                          id              bigserial primary key,
-                         productRefId    bigint not null,
-                         customerRefId   bigint not null,
-                         managerRefId    bigint not null,
                          source_id       bigint not null,
                          comment         text,
                          status          varchar(255) not null,
