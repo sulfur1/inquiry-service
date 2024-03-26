c6a9f6bf681888a1c9ab8dad0fc9e0b897b2779b
sobyanin2077 <kieferrosita9@gmail.com>
2024-03-26 18:29:01 +0300

    Fixed star-imports, exception handling and repeating string literals checkstyle violations, checkctyle:check goal still does not pass correctly though (Missing package-info.java file, Utility classes should not have a public or default constructor, Missing a Javadoc comment, Parameter args should be final)

--- inquiry-service-app/src/main/java/com/iprody08/inquiryservice/aspect/MyLoggingAspect.java
+++ inquiry-service-app/src/main/java/com/iprody08/inquiryservice/aspect/MyLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.iprody08.inquiryservice.aspect;
 
+import lombok.SneakyThrows;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyLoggingAspect {
 
+    @SneakyThrows
     @Around("execution(* com.iprody08.inquiryservice.dao.*.*(..))")
-    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) {
         final MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         final String methodName = methodSignature.getName();
         System.out.println("Begin of " + methodName);
-        final Object targetMethodResult = proceedingJoinPoint.proceed();
+        final Object targetMethodResult;
+        targetMethodResult = proceedingJoinPoint.proceed();
         System.out.println("End of " + methodName);
 
         return targetMethodResult;
--- inquiry-service-app/src/main/java/com/iprody08/inquiryservice/controller/SourceController.java
+++ inquiry-service-app/src/main/java/com/iprody08/inquiryservice/controller/SourceController.java
@@ -5,7 +5,14 @@ import com.iprody08.inquiryservice.exception_handlers.NotFoundException;
 import com.iprody08.inquiryservice.service.SourceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,6 +20,8 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public final class SourceController {
     private final SourceService sourceService;
+
+    private final String noSourceWithIdString = "There is no Source with id";
 
     public SourceController(final SourceService sourceService) {
         this.sourceService = sourceService;
@@ -26,7 +35,7 @@ public final class SourceController {
     @GetMapping("/sources/id/{id}")
     public SourceDto findById(@PathVariable long id) {
         return sourceService.findById(id)
-                .orElseThrow(() -> new NotFoundException("There is no Source with id " + id));
+                .orElseThrow(() -> new NotFoundException(noSourceWithIdString + id));
     }
 
     @DeleteMapping("/sources/id/{id}")
@@ -44,7 +53,7 @@ public final class SourceController {
     public ResponseEntity<SourceDto> update(@PathVariable long id, @RequestBody SourceDto sourceDto) {
         return sourceService.update(id, sourceDto)
                 .map(ResponseEntity::ok)
-                .orElseThrow(() -> new NotFoundException("There is no Source with id " + id));
+                .orElseThrow(() -> new NotFoundException(noSourceWithIdString + id));
     }
 
 }
